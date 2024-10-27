9e4a359f0b43d6dba042f83315e62c8fbf801c4b
Tanush200 <tanush.saha05@gmail.com>
2024-10-28 01:38:08 +0530

    committed

--- Authentication/index.js
+++ Authentication/index.js
@@ -1,34 +1,34 @@
 const express = require("express");
 const app = express();
 
-app.use(express.json());
-const gernerateTokens = ()=>{
-    return Math.random();
+app.use(express.json());  // Middleware
 
+const gernerateTokens = ()=>{ // token generator Function
+    return Math.random();
 }
 
-const users = [];
+const users = [];   // Array for Storing username & password
 
-app.post("/signup",(req,res)=>{
-    const username = req.body.username;
+app.post("/signup",(req,res)=>{  // HTTP
+    const username = req.body.username;  // Use middleware for displaying data in body from postman 
     const password = req.body.password;
 
 users.push({
-    username:username,
+    username:username,   // push username & password in users Array
     password:password
 })
 res.json({
-    message:"You are signed in"
+    message:"You are signed in"  // After signup...Display this message
 })
 
 })
 
 app.post("/signin",(req,res)=>{
-    const username = req.body.username;
+    const username = req.body.username;  // Use middleware for displaying data in body from postman After signup
     const password = req.body.password;
 
     const foundUser = users.find(function(u){
-        if(u.username == username && u.password == password){
+        if(u.username == username && u.password == password){  // If same username & password found return true
             return true;
         }
         else{
@@ -36,8 +36,8 @@ app.post("/signin",(req,res)=>{
         }
     })
     if(foundUser){
-        const token = gernerateTokens();
-        foundUser.token = token
+        const token = gernerateTokens();  // If find generate a token 
+        foundUser.token = token  // Store also the token in usersArray
 
         res.json({
              token:token

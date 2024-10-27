2cee6c5cbdea5b667db9b4416a674f2dc3dc3cd0
Tanush200 <tanush.saha05@gmail.com>
2024-10-28 00:41:38 +0530

    2nd

---
+++ Authentication/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.post("/signup",(req,res)=>{
+
+})
+
+app.post("/signin",(req,res)=>{
+    
+})
+
+app.listen(3000,()=>{
+    console.log("successful");
+})

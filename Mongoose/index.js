0320501a205673f0b9d5509cdd990169989f95e7
Tanush200 <tanush.saha05@gmail.com>
2024-11-13 11:09:21 +0530

    done

--- Mongoose/index.js
+++ Mongoose/index.js
@@ -14,62 +14,99 @@ const { UserModel , TodosModel } = require("./db")
 // Bcrypt
 const bcrypt = require("bcrypt");  // 1.import bcrypt library.....
 
+// Zod
+const {z} = require("zod");  // 0.1. import zod library
+
 // Middleware
 app.use(express.json());
 
 
 
 app.post("/signup",async function(req,res){
+   // Input Validation....
+    const reqBody = z.object({
+        email:z.string.min(5).max(100).email(),
+        password:z.string(),
+        name:z.string().min(5).max(100).email()
+    })
+    // const pasrsedData = reqBody.parse(req.body);
+    const pasrsedDataWithSuccess = reqBody.safeParse(req.body)
+
+    if(!pasrsedDataWithSuccess.success){
+         res.json({
+            message:"Incorrect Format",
+            error:pasrsedDataWithSuccess.error.errors  // User can see the error
+         }) 
+    }
+
+
     const name = req.body.name;
     const email = req.body.email;
     const password = req.body.password;
 
-    const hashedPassword =await bcrypt.hash(password,5)  // 2.hashing password
+    // Input validates.....
+    // if(typeof email !== "string" || email.length <5 || !email.includes("@")){
+    //     res.json({
+    //         message:"Email Incorrect"
+    //     })
+    //     return
+    // }
+    
+    try {
+        const hashedPassword =await bcrypt.hash(password,5)  // 2.hashing password
 
    await UserModel.create({       
         name:name,
         email:email,
         password:hashedPassword
-    })
-
+    });
     res.json({
         message:"You are Logged in"
     })
-
+    } catch (error) {
+        res.status(500).json({
+            message:"Error in Signed IN"
+        })
+    }
 })
 
 app.post("/login",async function(req,res){
     const email = req.body.email;
     const password = req.body.password;
+    
+    try{
+        const user =await UserModel.findOne({    // Read
+            email:email,
+        })
+    
+        if(!response){
+            res.status(404).json({
+                message:"No Response in db"
+            })
+        }
+         
+        const passwordMatched = bcrypt.compare(password,response.password); // 3.Compare the hashedPassword
+    
+        if(passwordMatched){
+            const token = jwt.sign({
+                id:user._id.toString()  // Because That id is a ObjectId..Convert to string
+            },JWT_SECRET)
+            res.json({
+                token:token
+            })
+        }
+        else{
+            res.status(404).json({
+                message:"Wrong Credintials"
+            })
+        }
 
-    const user =await UserModel.findOne({    // Read
-        email:email,
-        password:password
-    })
-
-    if(!response){
+    }catch(error){
         res.status(404).json({
-            message:"No Response in db"
+            message:"Error in Login"
         })
     }
-     
-    const passwordMatched = bcrypt.compare(password,response.password); // 3.Compare the hashedPassword
-
-    if(passwordMatched){
-        const token = jwt.sign({
-            id:user._id.toString()  // Because That id is a ObjectId..Convert to string
-        },JWT_SECRET)
-        res.json({
-            token:token
-        })
-    }
-    else{
-        res.status(404).json({
-            message:"Wrong Credintials"
-        })
-    }
-
-
+    
 })
 
 function auth(req,res,next){    // This middleware function checks or verify if user logged in or not
